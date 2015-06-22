b6d294e208926e3f6ec68a3ae654527434e82016
Dave Nash <dave.nash@teaandcode.com>
2015-06-22 12:45:56 +0100

    Added array checking from JSON
    * Created new step definition
    * Fixed failing Travis Behat test

--- src/Behat/GuzzleExtension/Context/RawGuzzleContext.php
+++ src/Behat/GuzzleExtension/Context/RawGuzzleContext.php
@@ -38,7 +38,7 @@ class RawGuzzleContext implements GuzzleAwareContext
     /**
      * @var string
      */
-    const GUZZLE_EXTENSION_VERSION = '0.3.5';
+    const GUZZLE_EXTENSION_VERSION = '0.3.6';
 
     /**
      * @var Client
